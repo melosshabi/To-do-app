06915b22148748dee539c5c1d77ada7aad4b5be9
melosshabi <melosshabi05@gmail.com>
2022-12-19 16:12:54 +0100

    Update
    Fixed some bugs with the app

--- index.js
+++ index.js
@@ -1,160 +1,161 @@
+var tasksArray = [];
+let taskInputsArray = [];
+let saveBtns = [];
+let editBtns = [];
+let deleteBtns = [];
 window.addEventListener('load', ()=>{
-    let taskInput = document.getElementsByClassName('task-field')[0];
-    let taskList = document.getElementsByClassName('tasks')[0];
-    let addTask = document.getElementsByClassName('add-task')[0];
-    let editBtn = document.getElementsByClassName('edit');
-    let deleteBtn = document.getElementsByClassName('delete');
-
-    let storageIndex = localStorage.length += 1;
-    console.log(storageIndex);
-
     if(localStorage.length != 0){
-        for(let i = 1; i <= localStorage.length; i++){
-            var newTask = document.createElement('div');
-            var newInput = document.createElement('input');
-            var newBtnDiv = document.createElement('div');
-            var newEdit = document.createElement('button');
-            var newDelete = document.createElement('button');
-           
-            let taskValue = localStorage.getItem(`Task${i}`);
+        tasksArray = JSON.parse(localStorage.getItem('list'));
+    }
+        tasksArray.map((task, index) =>{
+            index++;
+            let newTaskDiv = document.createElement('div');
+            let newBtnDiv = document.createElement('div');
+            let newInput = document.createElement('input');
+            let newSave = document.createElement('button');
+            let newEdit = document.createElement('button');
+            let newDelete = document.createElement('button');
 
-            newInput.value = taskValue;
+            newTaskDiv.className = "task";
+            newTaskDiv.setAttribute('name', `name${index}`)
+            newInput.className = "task-input";
+            newBtnDiv.className = "buttons-div";
             newInput.disabled = true;
-
-            newBtnDiv.classList.add('btn-div');
+            newInput.value = task;
+            newInput.setAttribute('name', `name${index}`)
+            newSave.className = "save";
+            newSave.classList.add("disabled");
+            newSave.innerText = "Save";
+            newSave.disabled = true;
+            newSave.setAttribute('name', `name${index}`)
+            newEdit.className = "edit";
+            newEdit.innerText = "Edit";
+            newEdit.setAttribute('name', `name${index}`)
+            newDelete.className = "delete";
+            newDelete.innerText = "Delete";
+            newDelete.setAttribute('name', `name${index}`)
+            
+            newTaskDiv.appendChild(newInput);
+            newTaskDiv.appendChild(newBtnDiv);
+            newBtnDiv.appendChild(newSave);
             newBtnDiv.appendChild(newEdit);
             newBtnDiv.appendChild(newDelete);
-            newEdit.textContent = "Edit";
-            newEdit.classList.add("edit");
-            newDelete.textContent = "Delete";
-            newDelete.classList.add("delete");
+            
+            tasksDiv.appendChild(newTaskDiv);
 
-            newTask.classList.add("task");
-            newTask.appendChild(newInput);
-            newTask.appendChild(newBtnDiv);
+            index--;
 
-            taskList.appendChild(newTask);
-    }
-        let editBtns = [...editBtn];
-        let deleteBtns = [...deleteBtn];
-
-    editBtns.forEach(btn => {
-        btn.addEventListener('click', ()=>{
-            if(newInput.disabled == true){
-                newInput.disabled = false;
-                newEdit.textContent = "Save";
-                var oldValue = newInput.value;
-            }
-            else if(newInput.disabled == false){
-                newInput.disabled = true;
-                newEdit.textContent = "Edit";
-                
-                console.log(oldValue);
-                for(let i = 0; i<= localStorage.length; i++){
-                    let storageValue = localStorage.getItem(localStorage.key(i));
-                    
-                    if(oldValue == storageValue){
-                        console.log("lol")
-                        localStorage.setItem(`Task${i}`, newInput.value)
-                    }
-
-                }
-            }
-        })
-    });
-
-    deleteBtns.forEach(btn =>{
-        btn.addEventListener('click', ()=>{
-            for(let i = 1; i <= localStorage.length; i++){
-                let storageValue = localStorage.getItem(`Task${i}`);
-                if(newInput.value == storageValue){
-                      newTask.remove();
-                      localStorage.removeItem(`Task${i}`);
-                      storageIndex = localStorage.length += 1;
-                }
-            }
-        })
-    });
+            let taskInputs = document.getElementsByClassName('task-input')[index];
+            let saveBtn = document.getElementsByClassName('save')[index];
+            let editBtn = document.getElementsByClassName('edit')[index];
+            let deleteBtn = document.getElementsByClassName('delete')[index];
+            taskInputsArray.push(taskInputs);
+            saveBtns.push(saveBtn);
+            editBtns.push(editBtn);
+            deleteBtns.push(deleteBtn);
            
-    }
-    
-    addTask.addEventListener('click', ()=>{
-        let newTask = document.createElement('div');
-        let newInput = document.createElement('input');
-        let newBtnDiv = document.createElement('div');
-        let newEdit = document.createElement('button');
-        let newDelete = document.createElement('button');
+        })
+        let prevInputValue;
+        editBtns.map((btn, index) =>{
+            
+            index++;
+            btn.addEventListener('click', ()=>{
+                let items = document.querySelectorAll(`[name="name${index}"]`);
+                let input = items[1];
+                let saveBtn = items[2];
+                saveBtn.classList.toggle("disabled");
+                saveBtn.disabled = false;
+                input.disabled = false;
+                prevInputValue = input.value;
+            })
+        })
+        saveBtns.map((btn, index) =>{
+            index++;
+            btn.addEventListener('click', ()=>{
+                let items = document.querySelectorAll(`[name="name${index}"]`);
+                let input = items[1];
+                btn.disabled = true;
+                btn.classList.toggle("disabled");
+                input.disabled = true;
+                for(let i = 0; i < tasksArray.length; i++){
+                    if(prevInputValue === tasksArray[i]){
+                        tasksArray[i] = input.value;
+                        localStorage.setItem('list', JSON.stringify(tasksArray));
+                    }
+                }
+            })
+        })
+        deleteBtns.map((btn, index) =>{
+            index++;
+            btn.addEventListener('click', ()=>{
+                let items = document.querySelectorAll(`[name="name${index}"]`);
+                let div = items[0];
+                let input = items[1];
+                for(let i = 0; i < tasksArray.length; i++){
+                    if(input.value === tasksArray[i]){
+                        tasksArray.splice(i, 1);
+                        localStorage.setItem('list', JSON.stringify(tasksArray));
+                        div.remove();
+                    }
+                }
+            })
+        })
+})
 
-        if(taskInput.value == ""){
-            alert("Please type in a task");
-        }else{
-            let newTaskValue = taskInput.value;
+let userInput = document.getElementsByClassName('user-input')[0];
+let addTask = document.getElementsByClassName('add-task')[0];
+let tasksDiv = document.getElementsByClassName('tasks-div')[0];
+let btnDiv = document.getElementsByClassName('buttons-div')[0];
 
+addTask.addEventListener('click', ()=>{
+    if(userInput.value == ""){
+        alert("Please enter a task");
+    }   
+    else{
+        if(tasksArray.length != 0){
+            for(let i = 0; i < tasksArray.length; i++){
+                if(userInput.value === tasksArray[i]){
+                    alert("Task already exists");   
+                }
+            }
+        }
+            let newTaskValue = userInput.value;
+            let newTaskDiv = document.createElement('div');
+            let newBtnDiv = document.createElement('div');
+            let newInput = document.createElement('input');
+            let newSave = document.createElement('button');
+            let newEdit = document.createElement('button');
+            let newDelete = document.createElement('button');
+                    
+            tasksArray.push(userInput.value);
+                    
+            localStorage.setItem('list', JSON.stringify(tasksArray));
+            newTaskDiv.className = "task";
+            newTaskDiv.setAttribute('name', `name${tasksArray.length}`)
+            newBtnDiv.className = "buttons-div";
+            newInput.className = "task-input";
             newInput.value = newTaskValue;
             newInput.disabled = true;
-
-            newBtnDiv.classList.add('btn-div');
+            newInput.setAttribute('name', `name${tasksArray.length}`)
+            newSave.className = "save";
+            newSave.classList.add("disabled");
+            newSave.innerText = "Save";
+            newSave.setAttribute('name', `name${tasksArray.length}`)
+            newEdit.className = "edit";
+            newEdit.innerText = "Edit";
+            newEdit.setAttribute('name', `name${tasksArray.length}`)
+            newDelete.className = "delete";
+            newDelete.innerText = "Delete";
+            newDelete.setAttribute('name', `name${tasksArray.length}`)
+                    
+            newTaskDiv.appendChild(newInput);
+            newTaskDiv.appendChild(newBtnDiv);
+            newBtnDiv.appendChild(newSave);
             newBtnDiv.appendChild(newEdit);
             newBtnDiv.appendChild(newDelete);
-            newEdit.textContent = "Edit";
-            newEdit.classList.add("edit");
-            newDelete.textContent = "Delete";
-            newDelete.classList.add("delete");
-
-            newTask.classList.add("task");
-            newTask.appendChild(newInput);
-            newTask.appendChild(newBtnDiv);
-
-            taskList.appendChild(newTask);
-            
-            localStorage.setItem(`Task${storageIndex}`, newInput.value);
-            storageIndex++;
-            console.log(storageIndex)
-        
-            let editBtns = [...editBtn];
-            let deleteBtns = [...deleteBtn];
-
-            editBtns.forEach(btn => {
-                btn.addEventListener('click', ()=>{
-                    if(newInput.disabled == true){
-                        newInput.disabled = false;
-                        newEdit.textContent = "Save";
-                        var oldValue = newInput.value;
-                    }
-                    else if(newInput.disabled == false){
-                        newInput.disabled = true;
-                        newEdit.textContent = "Edit";
-                        
-                        console.log(oldValue);
-                        for(let i = 0; i<= localStorage.length; i++){
-                            let storageValue = localStorage.getItem(localStorage.key(i));
-                            
-                            if(oldValue == storageValue){
-                                console.log("lol")
-                                localStorage.setItem(`Task${i}`, newInput.value)
-                            }
-        
-                        }
-                    }
-                })
-            });
-
-            deleteBtns.forEach(btn =>{
-                btn.addEventListener('click', ()=>{
-                    for(let i = 1; i <= localStorage.length; i++){
-                        let storageValue = localStorage.getItem(`Task${i}`);
-                        if(newInput.value == storageValue){
-                              newTask.remove();
-                              localStorage.removeItem(`Task${i}`);
-                              storageIndex = localStorage.length += 1;
-                        }
-                    }
-                })
-            });
-
-        }
-                
-       taskInput.value = "";
-    })
-
+                    
+            tasksDiv.appendChild(newTaskDiv);
+                    
+            window.location.reload();
+    }
 })
